3c68e4b0006b5f00f967ed876720790fb72f5549
CalokTI <72436270+CalokTI@users.noreply.github.com>
2023-02-12 20:10:20 +0100

    bit of cleanup

--- 02._Data_Formats/node/file_readers/txtReader.js
+++ 02._Data_Formats/node/file_readers/txtReader.js
@@ -1,7 +1,7 @@
 //Read text file and convert the data to an js object
 import fs from "fs";
 
-async function txtReader(filePath) {
+function txtReader(filePath) {
 	return new Promise((resolve, reject) => {
 		fs.readFile(filePath, "utf-8", (error, data) => {
 			if (error) {
--- 02._Data_Formats/node/file_readers/xmlReader.js
+++ 02._Data_Formats/node/file_readers/xmlReader.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import { XMLParser } from "fast-xml-parser";
 
-async function xmlReader(filePath) {
+function xmlReader(filePath) {
 	return new Promise((resolve, reject) => {
 		fs.readFile(filePath, "utf-8", (error, data) => {
 			if (error) {
