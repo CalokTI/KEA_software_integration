da0b5d706c3ee724e6b2363f90ad1c7b6a817699
CalokTI <72436270+CalokTI@users.noreply.github.com>
2023-02-12 16:07:55 +0100

    txt file reader added, some issues with csv(numbers, array)

--- 02._Data_Formats/node/file_readers/csvReader.js
+++ 02._Data_Formats/node/file_readers/csvReader.js
@@ -2,33 +2,33 @@ import fs from "fs";
 import { parse } from "csv-parse";
 
 class CsvReader {
-  constructor(filePath) {
-    this.filePath = filePath;
-    this.data = [];
-  }
+	constructor(filePath) {
+		this.filePath = filePath;
+		this.data = [];
+	}
 
-  read() {
-    return new Promise((resolve, reject) => {
-      fs.createReadStream(this.filePath)
-        .pipe(parse({ columns: true }))
-        .on("data", data => {
-          this.data.push(data);
-        })
-        .on("end", () => {
-          console.log("Finished reading CSV file.");
-          resolve(this.data);
-        })
-        .on("error", error => {
-          reject(error);
-        });
-    });
-  }
+	read() {
+		return new Promise((resolve, reject) => {
+			fs.createReadStream(this.filePath)
+				.pipe(parse({ columns: true }))
+				.on("data", (data) => {
+					this.data.push(data);
+				})
+				.on("end", () => {
+					console.log("Finished reading CSV file.");
+					resolve(this.data);
+				})
+				.on("error", (error) => {
+					reject(error);
+				});
+		});
+	}
 }
 
 async function csvReader(filePath) {
-  const csvReader = new CsvReader(filePath);
-  const csvData = await csvReader.read();
-  return csvData;
+	const csvReader = new CsvReader(filePath);
+	const csvData = await csvReader.read();
+	return csvData;
 }
 
 export default csvReader;
--- 02._Data_Formats/node/index.js
+++ 02._Data_Formats/node/index.js
@@ -1,21 +1,19 @@
 import { opendirSync, readFileSync } from "fs";
 import path from "path";
 import csvReader from "./file_readers/csvReader.js";
-
-async function main(filePath) {
-  const csvFileData = await csvReader(filePath);
-  console.log("csv:", csvFileData);
-}
+import txtReader from "./file_readers/txtReader.js";
 
 const dir = opendirSync("../files");
 for await (const entry of dir) {
-    console.log("Found file:", entry.name);
-    console.log("file type:", path.extname(entry.name));
-
-    if (path.extname(entry.name) === ".csv") {
-        main(`../files/${entry.name}`);
-    }
-    if (path.extname(entry.name) === ".json") {
-        console.log("JSON", JSON.parse(readFileSync(`../files/${entry.name}`)));
-    }
+	if (path.extname(entry.name) === ".csv") {
+		const csvFileData = await csvReader(`../files/${entry.name}`);
+		console.log("csv:", csvFileData);
+	}
+	if (path.extname(entry.name) === ".json") {
+		console.log("JSON", JSON.parse(readFileSync(`../files/${entry.name}`)));
+	}
+	if (path.extname(entry.name) === ".txt") {
+		const txtFileData = await txtReader(`../files/${entry.name}`);
+		console.log("txt:", txtFileData);
+	}
 }
